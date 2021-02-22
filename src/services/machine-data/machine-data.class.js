e7b8c2d6f1da5a62e674d3a02474db633ead719b
jihad khorfan <spyshow86@gmail.com>
2021-02-22 11:08:14 +0200

    adjust machine data for the bar gtaph chart

--- src/services/machine-data/machine-data.class.js
+++ src/services/machine-data/machine-data.class.js
@@ -1,7 +1,10 @@
+/* eslint-disable semi */
 /* eslint-disable no-unused-vars */
+const { presetPalettes } = require("@ant-design/colors");
 const pool = require("./../../db");
 const { separateObject } = require("../../utils/separateObject");
 const { removeA } = require("../../utils/removeA");
+var moment = require("moment");
 const {
   blue,
   red,
@@ -11,6 +14,7 @@ const {
   cyan,
   purple,
 } = require("@ant-design/colors");
+const { round } = require("./../../utils/round");
 
 exports.MachineData = class MachineData {
   constructor(options) {
@@ -18,130 +22,364 @@ exports.MachineData = class MachineData {
   }
 
   async find(params) {
-    let colors = [blue, red, yellow, lime, cyan, purple, volcano];
-    let result = {};
+    let totalwidth = 0;
+    let previousDate = "Previous time";
+    let selectedDate = "selected date";
+    let newResult = {};
+    let widthSensor = [];
+    let oldResult = {};
+    let oldMachineData, newMachineData;
+    let data = { xData: [], yData: [], y1Data: [] };
+    let option = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      // title: {
+      //   text: params.query.machine_name.replace(/_/g, " "),
+      //   textStyle: {
+      //     fontSize: 24,
+      //   },
+      //   left: "40%",
+      // },
+      legend: {
+        top: 20,
+        right: 150,
+        orient: "vertical",
+        data: [previousDate, selectedDate],
+        formatter:
+          "{name} (" +
+          moment(params.query.newStartDate).from(params.query.oldStartDate) +
+          ")",
+      },
+      grid: [
+        {
+          top: 100,
+          bottom: 201,
+        },
+        {
+          height: 160,
+          bottom: 40,
+        },
+      ],
+      toolbox: {
+        show: true,
+        orient: "vertical",
+        left: "right",
+        top: "center",
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ["line", "bar", "stack", "tiled"] },
+          restore: { show: true },
+          saveAsImage: { show: true },
+        },
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: data.xData,
+          gridIndex: 0,
+          axisLabel: {
+            show: true,
+            rotate: 90,
+            align: "right",
+            verticalAlign: "middle",
+            position: "insideBottom",
+            padding: [0, 0, 0, 0],
+            backgroundColor: "red",
+            color: "#333",
+            fontSize: 16,
+            rich: {
+              name: {},
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              color: "#e7e7e7",
+            },
+          },
+          axisTick: {
+            lineStyle: {
+              color: "#e7e7e7",
+            },
+          },
+          zlevel: 2,
+        },
+        {
+          type: "category",
+          gridIndex: 1,
+          axisLine: {
+            show: false,
+          },
+          zlevel: 1,
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          gridIndex: 0,
+          axisLabel: {
+            color: "#333",
+          },
+          splitLine: {
+            lineStyle: {
+              type: "dashed",
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              color: "#ccc",
+            },
+          },
+          textStyle: {},
+          axisTick: {
+            lineStyle: {
+              color: "#ccc",
+            },
+          },
+        },
+        {
+          type: "value",
+          gridIndex: 1,
+          axisLabel: {
+            show: false,
+          },
+          axisLine: {
+            show: false,
+          },
+          splitLine: {
+            show: false,
+          },
+          axisTick: {
+            show: false,
+          },
+        },
+      ],
+      series: [
+        {
+          name: previousDate,
+          data: data.yData,
+          type: "bar",
+          label: {
+            show: true,
+            position: "top",
+            textStyle: {
+              verticalAlign: "bottom",
+              align: "center",
+              color: "#555",
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: (params) => {
+                let colors = [
+                  "#b6c2ff",
+                  "#96edc1",
+                  "#fcb75b",
+                  "#b6c2ff",
+                  "#96edc1",
+                  "#fcb75b",
+                  "#b6c2ff",
+                  "#96edc1",
+                  "#fcb75b",
+                  "#b6c2ff",
+                  "#96edc1",
+                  "#fcb75b",
+                ];
+                return colors[params.dataIndex];
+              },
+            },
+          },
+          xAxisIndex: 0,
+          yAxisIndex: 0,
+        },
+        {
+          name: selectedDate,
+          data: data.y1Data,
+          type: "bar",
+          label: {
+            show: true,
+            position: "top",
+            textStyle: {
+              verticalAlign: "middle",
+              align: "center",
+              color: "#555",
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: (params) => {
+                let colors = [
+                  "#4150d8",
+                  "#28bf7e",
+                  "#ed7c2f",
+                  "#4150d8",
+                  "#28bf7e",
+                  "#ed7c2f",
+                  "#4150d8",
+                  "#28bf7e",
+                  "#ed7c2f",
+                  "#4150d8",
+                  "#28bf7e",
+                  "#ed7c2f",
+                ];
+                return colors[params.dataIndex];
+              },
+            },
+          },
+          xAxisIndex: 0,
+          yAxisIndex: 0,
+        },
+      ],
+    };
+
+    /********************/
+    /* TODO: merge two queries into one */
+    // select DISTINCT ON (created_at)
+    // created_at,sum(MX4_LNM)
+    // from public."MX4_M22"
+    // group by created_at
+    // let query = `select DISTINCT ON (created_at) created_at,`;
+    // params.query.machine_sensors.sensors.map((sensor, i) => {
+    //   sensor.counter.map((counter) => {
+    //     query += sensor.id + "_" + counter.id+",";
+    //   });
+    //   // to esqape inspected, rejected and mold fields
+    // });
+    /********************/
+
+    //querying old data
+
+    await pool
+      .query(
+        `SELECT * FROM public."${params.query.machine_name}" WHERE created_at BETWEEN '${params.query.oldStartDate}' AND '${params.query.oldEndDate}'`
+      )
+      .then((res) => {
+        if (res.rows.length !== 0) {
+          oldResult = res.rows.reduce(function (previousValue, currentValue) {
+            let array = [];
+            let keys = Object.keys(res.rows[0]);
+            let keysUpdated = removeA(
+              keys,
+              "id",
+              "created_at",
+              "updated_at",
+              "machine_id"
+            );
+            keysUpdated.map((test) => {
+              return (array[test] = previousValue[test] + currentValue[test]);
+            });
+
+            return array;
+          });
+        }
+      });
+
+    //querying new data
     await pool
       .query(
         `SELECT * FROM public."${params.query.machine_name}" WHERE created_at BETWEEN '${params.query.startDate}' AND '${params.query.endDate}'`
       )
       .then((res) => {
-        result = res.rows.reduce(function (previousValue, currentValue) {
-          let array = [];
-          let keys = Object.keys(res.rows[0]);
-          let keysUpdated = removeA(
-            keys,
-            "id",
-            "created_at",
-            "updated_at",
-            "machine_id"
-          );
-          keysUpdated.map((test) => {
-            return (array[test] = previousValue[test] + currentValue[test]);
+        if (res.rows.length !== 0) {
+          newResult = res.rows.reduce(function (previousValue, currentValue) {
+            let array = [];
+            let keys = Object.keys(res.rows[0]);
+            let keysUpdated = removeA(
+              keys,
+              "id",
+              "created_at",
+              "updated_at",
+              "machine_id"
+            );
+            keysUpdated.map((test) => {
+              return (array[test] = previousValue[test] + currentValue[test]);
+            });
+
+            return array;
           });
-
-          return array;
-        });
-      });
-    let machineData = separateObject(result);
-    let chartData = params.query.machine_sensors.sensors.map((sensor, i) => {
-      let data = {
-        label: { formatter: "{b}: {c}" },
-        name: sensor.id,
-        itemStyle: {
-          color: colors[i][9],
-        },
-        children: [],
-      };
-      let colorCounter = 0;
-
-      sensor.counter.forEach((defect, index) => {
-        let value = 0;
-        machineData.forEach((obj) => {
-          if (Object.keys(obj)[0].indexOf(defect.id.toLowerCase()) > -1) {
-            value = obj[Object.keys(obj)[0]];
-          }
-          // console.log(
-          //   defect,
-          //   value,
-          //   obj,
-          //   defect.id.toLowerCase(),
-          //   Object.keys(obj)[0].search(defect.id.toLowerCase())
-          // );
-        });
-        data.children[index] = {
-          label: { formatter: "{b}: {c}" },
-          name: defect.id,
-          value: value,
-
-          itemStyle: {
-            color: colors[i][8 - colorCounter],
-            shadowBlur: 2,
-            shadowColor: colors[2],
-          },
-        };
-        if (value !== 0) {
-          colorCounter++;
         }
       });
-      return data;
-    });
-    let option = {
-      tooltip: {
-        trigger: "item",
-        formatter: "{a} <br/>{b} : {c} ({d}%)",
-      },
-      title: {
-        id: params.query.machine_name,
-        text: `Total inspected: ${
-          machineData[0].inspected
-        } , Rejected: ${Math.round(
-          (machineData[1].rejected * 100) / machineData[0].inspected
-        )} %`,
-        textStyle: {
-          fontSize: 16,
-          align: "center",
-        },
-      },
-      series: {
-        id: params.query.machine_name,
-        name: params.query.machine_name,
-        type: "sunburst",
-        highlightPolicy: "ancestor",
-        data: chartData,
-        radius: ["00%", "90%"],
-        sort: null,
+    newMachineData = separateObject(newResult);
+    oldMachineData = separateObject(oldResult);
 
-        // label: {
-        //   formatter: "{b}: {c}",
-        // },
-        emphasis: {
-          label: {
-            formatter: "{b}: {c}",
+    let newPrecentage =
+      (newMachineData[1].rejected * 100) / newMachineData[0].inspected;
+    let oldPrecentage =
+      oldMachineData[1] === undefined
+        ? 0
+        : (oldMachineData[1].rejected * 100) / oldMachineData[0].inspected;
+    params.query.machine_sensors.sensors.forEach((sensor, i) => {
+      let width = 0;
+      sensor.counter.forEach((defect, index) => {
+        let newData, oldData;
+        newMachineData.forEach((obj) => {
+          if (Object.keys(obj)[0].indexOf(defect.id.toLowerCase()) > -1) {
+            newData = obj[Object.keys(obj)[0]];
+          }
+        });
+        oldMachineData.forEach((obj) => {
+          if (Object.keys(obj)[0].indexOf(defect.id.toLowerCase()) > -1) {
+            oldData = obj[Object.keys(obj)[0]];
+          }
+        });
+        if ((newData && newData !== 0) || (oldData && oldData !== 0)) {
+          data.y1Data.push(newData);
+          data.yData.push(oldData);
+          data.xData.push(defect.id);
+          //to calcualte the total width of the sensors
+          width++;
+          totalwidth++;
+          console.log(width, totalwidth, sensor.id, defect.id);
+        }
+      });
+      let sensorData = {
+        min: 1,
+        label: {
+          show: true,
+          position: "insideBottom",
+          formatter: "{b}",
+          offset: [0, 0],
+          textStyle: {
+            color: "#777",
           },
         },
-        downplay: {
-          label: {
-            formatter: "{b}: {c}",
+        type: "bar",
+        barGap: 0,
+        barWidth: 100 * width,
+        itemStyle: {
+          normal: {
+            color: presetPalettes.blue[i + 2],
           },
         },
-        levels: [
-          {
-            // Blank setting for data mining
-          },
-          {
-            // The most inside level
-            label: { formatter: "{b}", rotate: "tangential" },
-          },
-          {
-            // The second level
-            label: { formatter: "{b}: {c}", rotate: "radial" },
-          },
-        ],
-      },
-    };
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+      };
+      console.log(sensorData.itemStyle.normal.color);
+      sensorData.data = [
+        {
+          name: sensor.id.replace(/_/g, " "),
+          value: 1,
+        },
+      ];
+      option.series.push(sensorData);
+    });
+    //to remove the sensor if it has no data & calculate the total width of the sensor bar width
+    option.series.forEach((sensor, i) => {
+      if (sensor.barWidth === 0) {
+        option.series.splice(i, 1);
+      } else {
+        sensor.barWidth = sensor.barWidth / totalwidth + "%";
+      }
+    });
 
     return {
       data: [option],
+      newPrecentage: round(newPrecentage, 2),
+      oldPrecentage: round(oldPrecentage, 2),
     };
   }
 
