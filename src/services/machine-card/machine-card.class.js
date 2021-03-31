56ffd579672487160d42999948912c4ee384cc5e
jihad khorfan <spyshow86@gmail.com>
2021-03-31 18:13:11 +0300

    sql return data of rejection for 5 min intrval

--- src/services/machine-card/machine-card.class.js
+++ src/services/machine-card/machine-card.class.js
@@ -1,36 +1,54 @@
 /* eslint-disable no-unused-vars */
+const pool = require("./../../db");
+var moment = require("moment");
 exports.MachineCard = class MachineCard {
-  constructor (options) {
+  constructor(options) {
     this.options = options || {};
   }
 
-  async find (params) {
+  async find(params) {
     return [];
   }
 
-  async get (id, params) {
+  async get(id, params) {
+    //console.log(params.sequelize.include.model);3
+    console.log(params.query);
+    await pool
+      .query(
+        //get sum of inspected and rejected every 5 min (5 * 60 = 300)
+        `SELECT sum(inspected) as inspected,sum(rejected) as rejected,  
+        to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
+         as interval_alias
+        FROM public."${params.query.machine_name}_${params.query.line_number}" GROUP BY interval_alias
+        order by interval_alias`
+      )
+      .then((res) => {
+        console.log(res);
+      });
+
     return {
-      id, text: `A new message with ID: ${id}!`
+      id,
+      text: `A new message with ID: ${id}!`,
     };
   }
 
-  async create (data, params) {
+  async create(data, params) {
     if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+      return Promise.all(data.map((current) => this.create(current, params)));
     }
 
     return data;
   }
 
-  async update (id, data, params) {
+  async update(id, data, params) {
     return data;
   }
 
-  async patch (id, data, params) {
+  async patch(id, data, params) {
     return data;
   }
 
-  async remove (id, params) {
+  async remove(id, params) {
     return { id };
   }
 };
--- src/services/machine-card/machine-card.hooks.js
+++ src/services/machine-card/machine-card.hooks.js
@@ -1,14 +1,26 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate("jwt")],
     find: [],
-    get: [],
+    get: [
+      async (context) => {
+        //console.log(context);
+
+        // Get the Sequelize instance. In the generated application via:
+        const line_number = await context.app
+          .service("lines")
+          .get(context.params.query.lineId);
+        //console.log(line_number.line_number);
+        context.params.query.line_number = line_number.line_number;
+        return context;
+      },
+    ],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   after: {
@@ -18,7 +30,7 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -28,6 +40,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
