f79fa01c12b4488393151ee3a6f8bf62fb7107e4
jihad khorfan <spyshow86@gmail.com>
2021-04-01 08:53:16 +0300

    sql returns result with the required time range

--- src/services/machine-card/machine-card.class.js
+++ src/services/machine-card/machine-card.class.js
@@ -12,14 +12,19 @@ exports.MachineCard = class MachineCard {
 
   async get(id, params) {
     //console.log(params.sequelize.include.model);3
-    console.log(params.query);
+    console.log(params.query.oldDate);
+    console.log(moment().subtract(params.query.oldDate, "hours").format());
     await pool
       .query(
         //get sum of inspected and rejected every 5 min (5 * 60 = 300)
         `SELECT sum(inspected) as inspected,sum(rejected) as rejected,  
         to_timestamp(floor((extract('epoch' from created_at) / 300 )) * 300) 
          as interval_alias
-        FROM public."${params.query.machine_name}_${params.query.line_number}" GROUP BY interval_alias
+        FROM public."${params.query.machine_name}_${params.query.line_number}" 
+        where created_at > '${moment()
+          .subtract(params.query.oldDate, "hours")
+          .format()}'
+        GROUP BY interval_alias
         order by interval_alias`
       )
       .then((res) => {
