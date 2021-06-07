ab2582cd983b5e476d51802d6e18bffbd1691b67
jihad khorfan <spyshow86@gmail.com>
2021-06-07 15:15:53 +0300

    adjust linespeed find api

--- src/services/linespeed/linespeed.class.js
+++ src/services/linespeed/linespeed.class.js
@@ -13,7 +13,7 @@ exports.Linespeed = class Linespeed extends Service {
       .service("lines")
       .find()
       .then(async (lines) => {
-        console.log(lines.data[0].dataValues);
+        console.log(lines.data[0].dataValues.line_number);
         for (let i = 0; i < lines.data.length; i++) {
           await this.app
             .service("jobs")
@@ -28,8 +28,9 @@ exports.Linespeed = class Linespeed extends Service {
               },
             })
             .then(async (job) => {
-              if (job.length > 0) {
-                console.log(job.data);
+              console.log(job);
+              if (job.data.length > 0) {
+                console.log(lines.data[i].dataValues.id, job.data[0].speed);
                 //add speed for each line to linespeed table
                 await this.app.service("linespeed").create({
                   lineId: lines.data[i].dataValues.id,
@@ -40,11 +41,12 @@ exports.Linespeed = class Linespeed extends Service {
             .catch((error) => console.log("jobs: ", error));
         }
         //delete rows older than 2 hours
+        console.log(moment().subtract(2, "hours").format());
         this.app
           .service("linespeed")
           .remove(null, {
             query: {
-              createdAt: { $gte: moment().subtract(2, "hours").toDate() },
+              createdAt: { $lte: moment().subtract(2, "hours").format() },
             },
             multi: true,
           })
