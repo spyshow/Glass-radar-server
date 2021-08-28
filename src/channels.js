af8ae295b354c67ba1760ae19d1ba1d2a131fdb5
jihad khorfan <spyshow86@gmail.com>
2021-08-28 08:55:57 +0200

    change console.log

--- src/channels.js
+++ src/channels.js
@@ -35,17 +35,17 @@ module.exports = function (app) {
   //     // app.channel(`userIds/$(user.id}`).join(channel);
   //   }
   // });
- 
+
   // eslint-disable-next-line no-unused-vars
   app.publish((data, hook) => {
     // Here you can add event publishers to channels set up in `channels.js`
     // To publish only for a specific event use `app.publish(eventname, () => {})`
 
-    console.log(
-      "Publishing all events to all authenticated users. ",
-      hook.path,
-      data
-    ); // eslint-disable-line
+    // console.log(
+    //   "Publishing all events to all authenticated users. ",
+    //   hook.path,
+    //   data
+    // ); // eslint-disable-line
     return app.channel("anonymous");
     // e.g. to publish all service events to all authenticated users use
     ////return app.channel("authenticated");
