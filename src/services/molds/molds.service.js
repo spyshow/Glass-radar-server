501760e4eeb2e661ee75d33b73cf772d119d4a31
jihad khorfan <spyshow86@gmail.com>
2021-12-22 10:20:18 +0200

    adjust option to include multi

--- src/services/molds/molds.service.js
+++ src/services/molds/molds.service.js
@@ -1,19 +1,21 @@
+/* eslint-disable linebreak-style */
 // Initializes the `molds` service on path `/molds`
-const { Molds } = require('./molds.class');
-const createModel = require('../../models/molds.model');
-const hooks = require('./molds.hooks');
+const { Molds } = require("./molds.class");
+const createModel = require("../../models/molds.model");
+const hooks = require("./molds.hooks");
 
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
+    multi: ["create", "update", "patch", "remove"],
   };
 
   // Initialize our service with any options it requires
-  app.use('/molds', new Molds(options, app));
+  app.use("/molds", new Molds(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('molds');
+  const service = app.service("molds");
 
   service.hooks(hooks);
 };
--- src/services/moldstatus/moldstatus.service.js
+++ src/services/moldstatus/moldstatus.service.js
@@ -1,19 +1,20 @@
 // Initializes the `moldstatus` service on path `/moldstatus`
-const { Moldstatus } = require('./moldstatus.class');
-const createModel = require('../../models/moldstatus.model');
-const hooks = require('./moldstatus.hooks');
+const { Moldstatus } = require("./moldstatus.class");
+const createModel = require("../../models/moldstatus.model");
+const hooks = require("./moldstatus.hooks");
 
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
+    multi: ["create", "update", "patch", "remove"],
   };
 
   // Initialize our service with any options it requires
-  app.use('/moldstatus', new Moldstatus(options, app));
+  app.use("/moldstatus", new Moldstatus(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('moldstatus');
+  const service = app.service("moldstatus");
 
   service.hooks(hooks);
 };
