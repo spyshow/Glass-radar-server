6b114a14f7a868ece77d3e7f63df3bb305dce8bb
jihad khorfan <spyshow86@gmail.com>
2021-12-22 10:20:01 +0200

    adjust end of line to crlf

--- src/services/moldsets/moldsets.service.js
+++ src/services/moldsets/moldsets.service.js
@@ -1,19 +1,19 @@
 // Initializes the `moldsets` service on path `/moldsets`
-const { Moldsets } = require('./moldsets.class');
-const createModel = require('../../models/moldsets.model');
-const hooks = require('./moldsets.hooks');
+const { Moldsets } = require("./moldsets.class");
+const createModel = require("../../models/moldsets.model");
+const hooks = require("./moldsets.hooks");
 
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
   };
 
   // Initialize our service with any options it requires
-  app.use('/moldsets', new Moldsets(options, app));
+  app.use("/moldsets", new Moldsets(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('moldsets');
+  const service = app.service("moldsets");
 
   service.hooks(hooks);
 };
